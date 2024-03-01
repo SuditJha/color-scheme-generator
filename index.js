4b1cbd8ba8212821aa5b449481714a20a092b6b7
Sudit Kumar Jha <jsudit2@gmail.com>
2024-03-01 05:08:18 +0000

    completed color generation

--- index.js
+++ index.js
@@ -21,14 +21,17 @@ function renderColors(colors){
     let html = ''
     for( color of colors){
         html += `
-            <div class="colomn">
-                <div class="color" style="background-color:${color.hex.value};"></div>
-                <h4>${color.hex.value}</h4>
-            </div>
+        <div class="column" id="${color.hex.value}">
+            <div class="color" style="background-color:${color.hex.value};"></div>
+            <h4 class="hex-code" >${color.hex.value}</h4>
+        </div>
         `
     }
     document.getElementById('color-scheme-container').innerHTML = html
 
 }
 
-
+{/* <div class="colomn">
+                <div class="color" style="background-color:${color.hex.value};"></div>
+                <h4 class="hex-code">${color.hex.value}</h4>
+            </div> */}
